ae623a78ebc977b3d0108d19c41b8b2b5b0f9cd2
Erythrocyte3803 <2544390577@qq.com>
2023-10-19 20:30:22 +0900

    feat: update spklist api

--- src/api/spks.ts
+++ src/api/spks.ts
@@ -1,6 +1,6 @@
 import axios from "./axios"
 
 export default async () => {
-    const { data } = await axios.get("https://tts.ai-lab.top/list")
+    const { data } = await axios.get("https://tirs.ai-lab.top/spklist/spks.json")
     return data;
 }
